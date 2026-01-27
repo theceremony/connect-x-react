5f07f0df7824855bae3c78ad529dec75512b5b60
Jeramy Morrill <jeramymorrill@Jeramys-Mac-mini.local>
2026-01-27 15:07:02 -0500

    adding sockets

--- connext-x-game/src/components/game/Player/index.tsx
+++ connext-x-game/src/components/game/Player/index.tsx
@@ -3,16 +3,17 @@ import { useContext, useEffect, useState, type FC } from "react";
 import { StyledPlayer } from "./styled";
 import AppContext from "../../../App.context";
 import { type Player } from "../../../gameLogic/types";
+import { StyledSlot } from "../../scaffold";
 
 const Player: FC = () => {
   const { socket } = useContext(AppContext);
   const [player, setPlayer] = useState<Player | undefined>(undefined);
   useEffect(() => {
     if (socket) {
-      socket.on("game:player-joined-lobby", (val) => {
+      socket.on("game:player-joined-lobby", (val: Player) => {
         if (socket.id == val.id) setPlayer(val);
       });
-      socket.on("game:player-left-lobby", (val) => {
+      socket.on("game:player-left-lobby", (val: Player[]) => {
         val.forEach((v) => {
           if (v.id === socket.id) setPlayer(v);
         });
@@ -22,7 +23,13 @@ const Player: FC = () => {
   return (
     <StyledPlayer>
       <h1>PLAYER</h1>
-      {player && <div>id: {player.piece}</div>}
+      {player === undefined && <div>connecting...</div>}
+      {player && (
+        <div>
+          <StyledSlot data-slot-color={player.piece}></StyledSlot>
+          {player.piece}
+        </div>
+      )}
     </StyledPlayer>
   );
 };
