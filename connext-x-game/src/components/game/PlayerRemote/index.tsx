16a6aea90a87936c565c2cf3142458be9b487e80
Jeramy Morrill <jeramymorrill@Jeramys-Mac-mini.local>
2026-02-10 11:36:55 -0500

    player controls

--- connext-x-game/src/components/game/PlayerRemote/index.tsx
+++ connext-x-game/src/components/game/PlayerRemote/index.tsx
@@ -1,4 +1,4 @@
-import type { Player } from "@/gameLogic/types";
+import type { Game, Player } from "@/gameLogic/types";
 import { ROOM } from "@/netCode/config";
 import { socket } from "@/netCode/socket";
 import { type FC, useEffect, useEffectEvent, useState } from "react";
@@ -17,15 +17,20 @@ const PlayerRemote: FC = () => {
       if (socket?.id === player.id) setPlayer(player);
     },
   );
+
+  const onGameStatusUpdate = useEffectEvent(
+    ({ gameStatus }: { room: string; gameStatus: Game }) => {
+      setInGame(!!(gameStatus.players && gameStatus.players.length > 1));
+    },
+  );
+
   useEffect(() => {
     if (!player) socket?.emit("fp:request-connection", { room: ROOM });
-    socket?.on("tp:approve-connection", onApproveConnection);
-    socket.on("tap:game-status-update", (data) => {
-      if (data.gameStatus.players && data.gameStatus.players.length > 1)
-        setInGame(true);
-    });
+    socket.on("tp:approve-connection", onApproveConnection);
+    socket.on("tap:game-status-update", onGameStatusUpdate);
     return () => {
       socket?.removeListener("tp:approve-connection", onApproveConnection);
+      socket.removeListener("tap:game-status-update", onGameStatusUpdate);
       socket?.removeAllListeners();
     };
   }, []);
