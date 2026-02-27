6abaf29bd4c5e79a14ab065e4fed5e5f4de363b9
Jeramy Morrill <theceremonyends@gmail.com>
2026-02-27 07:00:29 -0500

    background cycle

--- connext-x-game/src/App.styled.ts
+++ connext-x-game/src/App.styled.ts
@@ -1,25 +1,9 @@
 import styled from "styled-components";
 
-import bkg1 from "./assets/funbkg-scifi-anime.png";
-import bkg2 from "./assets/funbkg-scifi-anime-beach.png";
-import bkg3 from "./assets/funbkg-scifi-anime-fantasy.png";
-import bkg4 from "./assets/funbkg-scifi-anime-cozy.png";
-import bkg5 from "./assets/funbkg-scifi-anime-spooky.png";
-import bkg6 from "./assets/funbkg-scifi-anime-monster.png";
-import bkg7 from "./assets/funbkg-scifi-anime-beach2.png";
-import bkg8 from "./assets/funbkg-scifi-anime-scifi2.png";
-import bkg9 from "./assets/bkg-no-theme.png";
-
-const randomValue = (arr: string[]) =>
-  arr[Math.floor(Math.random() * arr.length)];
-const backgrounds = [bkg1, bkg2, bkg3, bkg4, bkg5, bkg6, bkg7, bkg8, bkg9];
-
-const backdrop = randomValue(backgrounds);
-
-export const StyledApp = styled.div`
+export const StyledApp = styled.div<{ $backdrop?: string }>`
   display: flex;
   /* flex-direction: column; */
-  background: url(${backdrop});
+  background: url(${(props) => props.$backdrop});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
@@ -41,6 +25,8 @@ export const StyledApp = styled.div`
     line-height: 1;
   }
 
+  transition: background 3s;
+  transition-delay: 0.2s;
   &[data-is-player] {
     background: unset;
   }
--- connext-x-game/src/App.tsx
+++ connext-x-game/src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useReducer } from "react";
+import { lazy, Suspense, useReducer, useState } from "react";
 
 import AppContext from "./App.context";
 import { StyledApp, StyledLogo } from "./App.styled";
@@ -10,8 +10,36 @@ const GameController = lazy(() => import("@/components/game/GameController"));
 
 const PlayerRemote = lazy(() => import("@/components/game/PlayerRemote"));
 
+import { preload } from "react-dom";
+import { useInterval } from "react-use";
+import bkg9 from "./assets/bkg-no-theme.png";
+import bkg2 from "./assets/funbkg-scifi-anime-beach.png";
+import bkg7 from "./assets/funbkg-scifi-anime-beach2.png";
+import bkg4 from "./assets/funbkg-scifi-anime-cozy.png";
+import bkg3 from "./assets/funbkg-scifi-anime-fantasy.png";
+import bkg6 from "./assets/funbkg-scifi-anime-monster.png";
+import bkg8 from "./assets/funbkg-scifi-anime-scifi2.png";
+import bkg5 from "./assets/funbkg-scifi-anime-spooky.png";
+import bkg1 from "./assets/funbkg-scifi-anime.png";
+
+const randomValue = (arr: string[]) =>
+  arr[Math.floor(Math.random() * arr.length)];
+const backgrounds = [bkg1, bkg2, bkg3, bkg4, bkg5, bkg6, bkg7, bkg8, bkg9];
+
 function App() {
+  backgrounds.map((a) => {
+    preload(a, { as: "image" });
+  });
+
+  const [backdrop, setBackDrop] = useState<string>(randomValue(backgrounds));
   const [state, dispatch] = useReducer(appReducer, initialState);
+
+  useInterval(() => {
+    const newBackground = randomValue(backgrounds);
+    preload(newBackground, { as: "image" });
+    setBackDrop(newBackground);
+  }, 60000);
+
   const path = window.location.pathname;
   if (path === "/player")
     return (
@@ -25,7 +53,7 @@ function App() {
     );
   return (
     <AppContext.Provider value={{ state, dispatch }}>
-      <StyledApp>
+      <StyledApp $backdrop={backdrop}>
         <GameController />
         <StyledLogo src={logo} />
       </StyledApp>
