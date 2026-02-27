b2ab28828a614979fc64df5a7e5e6b8320c2d980
Jeramy Morrill <theceremonyends@gmail.com>
2026-02-27 00:28:19 -0500

    added winner loser sticker to test

--- connext-x-game/src/components/feedback/Message/index.tsx
+++ connext-x-game/src/components/feedback/Message/index.tsx
@@ -1,6 +1,9 @@
 import AppContext from "@/App.context";
 import { type FC, useContext } from "react";
-import { StyledMessage } from "./styled";
+import { StyledMessage, StyledWinnerSticker } from "./styled";
+
+import winnerRed from "../../../assets/winner-red.png";
+import winnerYellow from "../../../assets/winner-yellow.png";
 
 const Message: FC = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -14,6 +17,9 @@ const Message: FC = () => {
     <StyledMessage>
       <h1 className="large-message-headline">Winner!</h1>
       <h1>{state.currentGame?.winner}</h1>
+      <StyledWinnerSticker
+        src={state.currentGame?.winner === "red" ? winnerRed : winnerYellow}
+      />
       <button onClick={onNewGameClick}>New Game</button>
     </StyledMessage>
   );
--- connext-x-game/src/components/feedback/Message/styled.ts
+++ connext-x-game/src/components/feedback/Message/styled.ts
@@ -3,3 +3,8 @@ import { StyledModal } from "../../scaffold";
 export const StyledMessage = styled(StyledModal)`
   display: flex;
 `;
+
+export const StyledWinnerSticker = styled.img`
+  width: 100%;
+  max-width: 800px;
+`;
