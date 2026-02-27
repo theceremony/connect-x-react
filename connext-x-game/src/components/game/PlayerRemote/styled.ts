681bd4510dcc18dbd24a2aacb043171b557400b8
Jeramy Morrill <theceremonyends@gmail.com>
2026-02-27 03:10:19 -0500

    more css stuff

--- connext-x-game/src/components/game/PlayerRemote/styled.ts
+++ connext-x-game/src/components/game/PlayerRemote/styled.ts
@@ -14,6 +14,7 @@ export const StyledPlayer = styled.div`
 export const StyledTurnBlocker = styled.div`
   display: flex;
   position: fixed;
+  z-index: 100;
   width: 100%;
   height: 100%;
   align-items: center;
